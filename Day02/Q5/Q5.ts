5cbd807c349d8cd3508add527c970c063cba0630
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-04-21 18:43:53 +0500

    Add files via upload

---
+++ Day02/Q5/Q5.ts
@@ -0,0 +1,10 @@
+//Q5 - Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+//const famousQuote: string = "A person who never made a mistake never tried anything new.";
+//const author: string = "Albert Einstein";
+//console.log(`${author} once said, "${famousQuote}"`);
+
+const famous_person: string = 'Albert Einstein';
+const myQuote: string = `${famous_person} once said, "A person who never made a mistake never tried anything new."`;
+const message: string = myQuote;
+console.log(message);

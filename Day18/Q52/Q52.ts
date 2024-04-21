0a48bca9e32705201a7c37d74948e30ecd251da4
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-04-21 19:04:01 +0500

    Add files via upload

---
+++ Day18/Q52/Q52.ts
@@ -0,0 +1,17 @@
+// Q52 - Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+
+// Sets up details about a smartphone
+let smartphone = {
+    brand: "Samsung",
+    model: "Galaxy A02",
+    specs: {
+        Display: "6.5 inches", 
+        Weight:	"206 g",
+        Storage: "32GB / 64GB",
+        RAM: "2GB / 3GB",
+        Colors:	"Black, Blue, Red, White",
+        OperatingSystem: "Android 10 OS",
+        BatteryLife: "20 to 24 hours"
+    }
+};
+console.log(smartphone);
---
+++ Day18/Q54/Q54.js
@@ -0,0 +1,9 @@
+"use strict";
+// Q54 - Making Flexible Object Keys: Learn how to set up a list where you can change the name of each section based on what you need at that moment, like adjusting labels based on user choices.
+function createDynamicObject(key, value) {
+    let dynamicObject = {};
+    dynamicObject[key] = value;
+    return dynamicObject;
+}
+let userPreference = createDynamicObject("Country", "Pakistan");
+console.log(userPreference);

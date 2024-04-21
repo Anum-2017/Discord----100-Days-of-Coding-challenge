a6b12bbfda8bf6c4fb3b1f6e54e82ca9176720cc
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-04-21 18:43:20 +0500

    Add files via upload

---
+++ Day01/Q2/Q2.js
@@ -0,0 +1,5 @@
+"use strict";
+//Q2 - Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+let personName = "Anum";
+let message = `Hello ${personName}, Would you like to learn TypeScript today?`;
+console.log(message);
---
+++ Day01/Q2/Q2.ts
@@ -0,0 +1,5 @@
+//Q2 - Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let personName: string = "Anum"
+let message: string = `Hello ${personName}, Would you like to learn TypeScript today?`
+console.log(message)

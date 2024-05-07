d803e6a11bb3776b2f708023f1c35eaceec2ec2c
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-05-07 16:12:48 +0500

    Add files via upload

---
+++ Day40/Q120/Q120.js
@@ -0,0 +1,11 @@
+"use strict";
+//Q120 - Use a for...of loop to iterate through an array of your favorite movies and log each one.
+const favoriteMovies = [
+    "Harry Potter Series",
+    "The Matrix",
+    "The Dark Knight",
+    "Titanic"
+];
+for (const movie of favoriteMovies) {
+    console.log(movie);
+}
---
+++ Day40/Q120/Q120.ts
@@ -0,0 +1,12 @@
+//Q120 - Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+const favoriteMovies: string[] = [
+    "Harry Potter Series", 
+    "The Matrix", 
+    "The Dark Knight", 
+    "Titanic"];
+
+for (const movie of favoriteMovies) {
+    
+    console.log(movie);
+}

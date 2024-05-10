5894d37d0a805566b6526c22bec954bb90b423b4
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-05-10 11:00:02 +0500

    Add files via upload

---
+++ Day46/Q136/Q136.js
@@ -0,0 +1,8 @@
+"use strict";
+//Q136 - Use console.log() to debug and track the value of a variable inside a loop.
+// Sample loop
+for (let i = 0; i <= 5; i++) {
+    const variableToTrack = i * 2;
+    console.log("Value of variableToTrack:", variableToTrack);
+    // Your other code here
+}
---
+++ Day46/Q137/Q137.js
@@ -0,0 +1,8 @@
+"use strict";
+//Q137 - Implement a try-catch block to handle potential errors in a block of code.
+try {
+    throw new Error("Something went wrong");
+}
+catch (error) {
+    console.log(error);
+}

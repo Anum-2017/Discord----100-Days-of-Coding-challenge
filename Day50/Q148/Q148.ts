721ae298abcdec061f2052fded750a94d455ec93
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-05-11 00:00:53 +0500

    Add files via upload

---
+++ Day50/Q148/Q148.ts
@@ -0,0 +1,6 @@
+//Q148 -  Demonstrate the use of the setTimeout() function to execute code after a delay.
+
+// Uses setTimeout to log a message after a 2-second delay
+setTimeout(() => {
+    console.log("This message is shown after a 2-second delay.");
+  }, 2000);
